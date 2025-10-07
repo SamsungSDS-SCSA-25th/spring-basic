452688813908959edc0d39ca626eb3a1a1502b22
Eric SHIM <163520887+Data-MaSTeRR@users.noreply.github.com>
2025-10-07 18:33:13 +0900

    Test: 회원 도메인 junit 코드

---
+++ src/test/java/com/example/spring_basic/member/MemberServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.spring_basic.member;
+
+import com.example.spring_basic.member.service.MemberService;
+import com.example.spring_basic.member.service.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member(1L, "Eric", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member findMember = memberService.findMember(member.getId());
+
+        // then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+
+}
