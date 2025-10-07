753d3f05b0d42d5c2ecf3828ce77d9cfb832f54b
Eric SHIM <163520887+Data-MaSTeRR@users.noreply.github.com>
2025-10-07 21:58:09 +0900

    Refactor: AppConfig 가독성 높이기

--- src/main/java/com/example/spring_basic/AppConfig.java
+++ src/main/java/com/example/spring_basic/AppConfig.java
@@ -1,6 +1,8 @@
 package com.example.spring_basic;
 
+import com.example.spring_basic.discount.DiscountPolicy;
 import com.example.spring_basic.discount.RateDiscountPolicy;
+import com.example.spring_basic.member.repo.MemberRepository;
 import com.example.spring_basic.member.repo.MemoryMemberRepository;
 import com.example.spring_basic.member.service.MemberService;
 import com.example.spring_basic.member.service.MemberServiceImpl;
@@ -12,11 +14,21 @@ public class AppConfig {
 
     // MemberService 인터페이스에 대한 구체적인 구현체 값 지정
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     // OrderService 인터페이스에 대한 구체적인 구현체 값 지정
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(
+                memberRepository(),
+                discountPolicy());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    public DiscountPolicy discountPolicy() {
+    // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
